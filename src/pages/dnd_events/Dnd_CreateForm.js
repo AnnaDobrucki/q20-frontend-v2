42fcca9575ce3a6562765f6ca25b924d21011270
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-03-11 15:14:03 +0000

    fix dndevents.module.css unrequired in event.js

--- src/pages/dnd_events/Dnd_CreateForm.js
+++ src/pages/dnd_events/Dnd_CreateForm.js
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Row, Col, Container, Alert } from "react-bootstrap";
-//import styles from "../../styles/DndEventsPage.module.css";
 import { useHistory } from "react-router-dom";
 import btnStyles from "../../styles/Button.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
